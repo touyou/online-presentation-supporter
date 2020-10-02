440070ae6c3f78f79a24ad4fe600927630cb99d1
touyou <fujiyou1121@gmail.com>
2020-10-02 16:10:52 +0900

    for test

--- src/pages/api/[imageId].ts
+++ src/pages/api/[imageId].ts
@@ -15,24 +15,24 @@ export default async function (req: NowRequest, res: NowResponse) {
   );
 
   const imageUrl = "https://" + imageId[0] + imageId[1];
-  console.log(imageUrl);
-  const img = new Image();
-  img.src = imageUrl;
-  img.onload = async (e) => {
-    const getImage = (image: HTMLImageElement) => {
-      const canvas = document.createElement("canvas");
-      canvas.width = image.naturalWidth;
-      canvas.height = image.naturalHeight;
-      const ctx = canvas.getContext("2d");
-      ctx.drawImage(image, 0, 0, canvas.width, canvas.height);
-      return ctx.getImageData(0, 0, canvas.width, canvas.height);
-    };
+  res.json({ url: imageUrl });
+  // const img = new Image();
+  // img.src = imageUrl;
+  // img.onload = async (e) => {
+  //   const getImage = (image: HTMLImageElement) => {
+  //     const canvas = document.createElement("canvas");
+  //     canvas.width = image.naturalWidth;
+  //     canvas.height = image.naturalHeight;
+  //     const ctx = canvas.getContext("2d");
+  //     ctx.drawImage(image, 0, 0, canvas.width, canvas.height);
+  //     return ctx.getImageData(0, 0, canvas.width, canvas.height);
+  //   };
 
-    const result = await calculateComplexity(
-      getImage(e.target as HTMLImageElement)
-    );
-    res.status(200).json({
-      value: result.data.payload,
-    });
-  };
+  //   const result = await calculateComplexity(
+  //     getImage(e.target as HTMLImageElement)
+  //   );
+  //   res.status(200).json({
+  //     value: result.data.payload,
+  //   });
+  // };
 }
