132cb5eff45a8c64dba5214fc67e9c91dda04699
touyou <fujiyou1121@gmail.com>
2020-10-11 17:12:45 +0900

    :+1: add graph view

--- src/components/speakerItems/drawsiness.tsx
+++ src/components/speakerItems/drawsiness.tsx
@@ -9,16 +9,60 @@ import {
   Input,
 } from "@chakra-ui/core";
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import {
+  Bar,
+  LineChart,
+  Line,
+  ReferenceLine,
+  XAxis,
+  YAxis,
+  BarChart,
+} from "recharts";
 
 interface Props {
   drawsiness: number[];
+}
+
+interface DrawsinessLog {
+  average: number;
+  sleeperCount: number;
+  count: number;
 }
 
 const Drawsiness = (props: Props) => {
   const { drawsiness } = props;
   const [isOpen, setOpen] = useState(false);
   const [threshold, setThreshold] = useState(0.25);
+  const [drawsinessLog, setDrawsinessLog] = useState<DrawsinessLog[]>([]);
+
+  useEffect(() => {
+    if (drawsiness.length > 0) {
+      const average =
+        drawsiness.reduce((val1, val2, _, __) => {
+          return val1 + val2;
+        }) / drawsiness.length;
+      const sleeperCount = drawsiness.filter((val, _, __) => {
+        return val <= threshold;
+      }).length;
+      const newLog = [
+        {
+          average: average,
+          sleeperCount: sleeperCount,
+          count: drawsiness.length,
+        } as DrawsinessLog,
+      ]
+        .concat(drawsinessLog)
+        .slice(0, 20);
+      setDrawsinessLog(newLog);
+    }
+  }, [drawsiness]);
+
+  const getReversedArray = () => {
+    const copyArray = drawsinessLog;
+    copyArray.reverse();
+    return copyArray;
+  };
 
   const getDrawsinessMessage = () => {
     const sleeper = drawsiness.filter((val, _, __) => {
@@ -51,6 +95,23 @@ const Drawsiness = (props: Props) => {
         Toggle Setting
       </Button>
       <Collapse mt={4} isOpen={isOpen}>
+        <LineChart width={300} height={200} data={getReversedArray()}>
+          <XAxis />
+          <YAxis />
+          <ReferenceLine y={threshold} label="Sleepy" stroke="red" />
+          <Line type="monotone" dataKey="average" stroke="#3F51B5" />
+        </LineChart>
+        <BarChart width={300} height={200} data={getReversedArray()}>
+          <XAxis />
+          <YAxis />
+          <Bar
+            dataKey="sleeperCount"
+            stackId="counter"
+            barSize={20}
+            fill="#e57373"
+          />
+          <Bar dataKey="count" stackId="counter" barSize={20} fill="#9CCC65" />
+        </BarChart>
         <FormControl>
           <FormLabel htmlFor="threshold">Threshold</FormLabel>
           <Input
