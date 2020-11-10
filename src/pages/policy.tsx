f2bb0fda189c6275ec9ac0408bec1922a202c63a
touyou <fujiyou1121@gmail.com>
2020-11-10 10:20:54 +0900

    :wrench: change policy

---
+++ src/pages/policy.tsx
@@ -0,0 +1,277 @@
+import React from "react";
+import {
+  Box,
+  Heading,
+  Flex,
+  Text,
+  Stack,
+  List,
+  ListItem,
+} from "@chakra-ui/core";
+
+const Policy = () => {
+  return (
+    <>
+      <Box
+        pos="fixed"
+        w="100%"
+        h="80px"
+        bg="blue.800"
+        p="4"
+        boxShadow="md"
+        zIndex={2}
+      >
+        <Flex align="flex-end" justifyContent="space-between">
+          <Heading color="gray.100">Online Lecture System</Heading>
+        </Flex>
+      </Box>
+      <Stack spacing={3} w="100%" bg="gray.100" p="100px">
+        <Text fontSize="2xl" fontWeight="bold">
+          Privacy Policy for Online Presentation Supporter
+        </Text>
+        <Text fontSize="md">
+          At Online Presentation Supporter, accessible from
+          https://online-presentation-supporter.vercel.app/, one of our main
+          priorities is the privacy of our visitors. This Privacy Policy
+          document contains types of information that is collected and recorded
+          by Online Presentation Supporter and how we use it.
+        </Text>
+
+        <Text fontSize="md">
+          If you have additional questions or require more information about our
+          Privacy Policy, do not hesitate to contact us.
+        </Text>
+
+        <Text fontSize="md">
+          This Privacy Policy applies only to our online activities and is valid
+          for visitors to our website with regards to the information that they
+          shared and/or collect in Online Presentation Supporter. This policy is
+          not applicable to any information collected offline or via channels
+          other than this website. Our Privacy Policy was created with the help
+          of the{" "}
+          <a href="https://www.privacypolicygenerator.info">
+            Privacy Policy Generator
+          </a>{" "}
+          and the{" "}
+          <a href="https://www.privacypolicies.com/privacy-policy-generator/">
+            Free Privacy Policy Generator
+          </a>
+          .
+        </Text>
+        <Text fontSize="lg" fontWeight="bold">
+          Consent
+        </Text>
+        <Text fontSize="md">
+          By using our website, you hereby consent to our Privacy Policy and
+          agree to its terms. For our Terms and Conditions, please visit the{" "}
+          <a href="https://www.privacypolicyonline.com/terms-conditions-generator/">
+            Terms & Conditions Generator
+          </a>
+          .
+        </Text>
+        <Text fontSize="lg" fontWeight="bold">
+          Information we collect
+        </Text>
+        <Text fontSize="md">
+          The personal information that you are asked to provide, and the
+          reasons why you are asked to provide it, will be made clear to you at
+          the point we ask you to provide your personal information.
+        </Text>
+        <Text fontSize="md">
+          If you contact us directly, we may receive additional information
+          about you such as your name, email address, phone number, the contents
+          of the message and/or attachments you may send us, and any other
+          information you may choose to provide.
+        </Text>
+        <Text fontSize="md">
+          When you register for an Account, we may ask for your contact
+          information, including items such as name, company name, address,
+          email address, and telephone number.
+        </Text>
+
+        <Text fontSize="lg" fontWeight="bold">
+          How we use your information
+        </Text>
+
+        <Text fontSize="md">
+          We use the information we collect in various ways, including to:
+        </Text>
+
+        <List styleType="disc">
+          <ListItem>Provide, operate, and maintain our webste</ListItem>
+          <ListItem>Improve, personalize, and expand our webste</ListItem>
+          <ListItem>Understand and analyze how you use our webste</ListItem>
+          <ListItem>
+            Develop new products, services, features, and functionality
+          </ListItem>
+          <ListItem>
+            Communicate with you, either directly or through one of our
+            partners, including for customer service, to provide you with
+            updates and other information relating to the webste, and for
+            marketing and promotional purposes
+          </ListItem>
+          <ListItem>Send you emails</ListItem>
+          <ListItem>Find and prevent fraud</ListItem>
+        </List>
+
+        <Text fontSize="lg" fontWeight="bold">
+          Log Files
+        </Text>
+
+        <Text fontSize="md">
+          Online Presentation Supporter follows a standard procedure of using
+          log files. These files log visitors when they visit websites. All
+          hosting companies do this and a part of hosting services' analytics.
+          The information collected by log files include internet protocol (IP)
+          addresses, browser type, Internet Service Provider (ISP), date and
+          time stamp, referring/exit pages, and possibly the number of clicks.
+          These are not linked to any information that is personally
+          identifiable. The purpose of the information is for analyzing trends,
+          administering the site, tracking users' movement on the website, and
+          gathering demographic information.
+        </Text>
+
+        <Text fontSize="lg" fontWeight="bold">
+          Cookies and Web Beacons
+        </Text>
+
+        <Text fontSize="md">
+          Like any other website, Online Presentation Supporter uses 'cookies'.
+          These cookies are used to store information including visitors'
+          preferences, and the pages on the website that the visitor accessed or
+          visited. The information is used to optimize the users' experience by
+          customizing our web page content based on visitors' browser type
+          and/or other information.
+        </Text>
+
+        <Text fontSize="md">
+          For more general information on cookies, please read{" "}
+          <a href="https://www.cookieconsent.com/what-are-cookies/">
+            "What Are Cookies" from Cookie Consent
+          </a>
+          .
+        </Text>
+
+        <Text fontSize="lg" fontWeight="bold">
+          Advertising Partners Privacy Policies
+        </Text>
+
+        <Text fontSize="md">
+          You may consult this list to find the Privacy Policy for each of the
+          advertising partners of Online Presentation Supporter.
+        </Text>
+        <Text fontSize="md">
+          Third-party ad servers or ad networks uses technologies like cookies,
+          JavaScript, or Web Beacons that are used in their respective
+          advertisements and links that appear on Online Presentation Supporter,
+          which are sent directly to users' browser. They automatically receive
+          your IP address when this occurs. These technologies are used to
+          measure the effectiveness of their advertising campaigns and/or to
+          personalize the advertising content that you see on websites that you
+          visit.
+        </Text>
+        <Text fontSize="md">
+          Note that Online Presentation Supporter has no access to or control
+          over these cookies that are used by third-party advertisers.
+        </Text>
+        <Text fontSize="lg" fontWeight="bold">
+          Third Party Privacy Policies
+        </Text>
+        <Text fontSize="md">
+          Online Presentation Supporter's Privacy Policy does not apply to other
+          advertisers or websites. Thus, we are advising you to consult the
+          respective Privacy Policies of these third-party ad servers for more
+          detailed information. It may include their practices and instructions
+          about how to opt-out of certain options.{" "}
+        </Text>
+        <Text fontSize="md">
+          You can choose to disable cookies through your individual browser
+          options. To know more detailed information about cookie management
+          with specific web browsers, it can be found at the browsers'
+          respective websites.
+        </Text>
+        <Text fontSize="lg" fontWeight="bold">
+          CCPA Privacy Rights (Do Not Sell My Personal Information)
+        </Text>
+        <Text fontSize="md">
+          Under the CCPA, among other rights, California consumers have the
+          right to:
+        </Text>
+        <Text fontSize="md">
+          Request that a business that collects a consumer's personal data
+          disclose the categories and specific pieces of personal data that a
+          business has collected about consumers.
+        </Text>
+        <Text fontSize="md">
+          Request that a business delete any personal data about the consumer
+          that a business has collected.
+        </Text>
+        <Text fontSize="md">
+          Request that a business that sells a consumer's personal data, not
+          sell the consumer's personal data.
+        </Text>
+        <Text fontSize="md">
+          If you make a request, we have one month to respond to you. If you
+          would like to exercise any of these rights, please contact us.
+        </Text>
+        <Text fontSize="lg" fontWeight="bold">
+          GDPR Data Protection Rights
+        </Text>
+        <Text fontSize="md">
+          We would like to make sure you are fully aware of all of your data
+          protection rights. Every user is entitled to the following:
+        </Text>
+        <Text fontSize="md">
+          The right to access – You have the right to request copies of your
+          personal data. We may charge you a small fee for this service.
+        </Text>
+        <Text fontSize="md">
+          The right to rectification – You have the right to request that we
+          correct any information you believe is inaccurate. You also have the
+          right to request that we complete the information you believe is
+          incomplete.
+        </Text>
+        <Text fontSize="md">
+          The right to erasure – You have the right to request that we erase
+          your personal data, under certain conditions.
+        </Text>
+        <Text fontSize="md">
+          The right to restrict processing – You have the right to request that
+          we restrict the processing of your personal data, under certain
+          conditions.
+        </Text>
+        <Text fontSize="md">
+          The right to object to processing – You have the right to object to
+          our processing of your personal data, under certain conditions.
+        </Text>
+        <Text fontSize="md">
+          The right to data portability – You have the right to request that we
+          transfer the data that we have collected to another organization, or
+          directly to you, under certain conditions.
+        </Text>
+        <Text fontSize="md">
+          If you make a request, we have one month to respond to you. If you
+          would like to exercise any of these rights, please contact us.
+        </Text>
+        <Text fontSize="lg" fontWeight="bold">
+          Children's Information
+        </Text>
+        <Text fontSize="md">
+          Another part of our priority is adding protection for children while
+          using the internet. We encourage parents and guardians to observe,
+          participate in, and/or monitor and guide their online activity.
+        </Text>
+        <Text fontSize="md">
+          Online Presentation Supporter does not knowingly collect any Personal
+          Identifiable Information from children under the age of 13. If you
+          think that your child provided this kind of information on our
+          website, we strongly encourage you to contact us immediately and we
+          will do our best efforts to promptly remove such information from our
+          records.
+        </Text>
+      </Stack>
+    </>
+  );
+};
+
+export default Policy;
